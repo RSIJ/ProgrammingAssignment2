3ab1490ab29699509246ff1d1d476e4c6e782b90
RSIJ <robert.ijnsen@gmail.com>
2019-03-08 08:52:33 +0100

    Correct the setInvMat-function in makeCacheMatrix. Adding descriptive comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions below create from an input matrix a "special" matrix, for
+## which the inverse will be cached. Caching data is an alternative for re-calculation.
 
-## Write a short comment describing this function
-
+## This function creates a list-object from a matrix as input variable. 
+## The list-object is able to cache the inverse of the input-matrix.
+## It contains 4 functions:
+## 1. set the value of the matrix.
+## 2. get the value of the matrix.
+## 3. set/calculate the inverse of the matrix.
+## 4. get the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
     im <- NULL
     set <- function(y) {
@@ -10,7 +15,7 @@ makeCacheMatrix <- function(x = matrix()) {
         im <<- NULL
     }
     get <- function() x
-    setInvMat <- function(invMat) im <<- solve
+    setInvMat <- function(solve) im <<- solve
     getInvMat <- function() im
     list(set = set, 
          get = get,
@@ -19,8 +24,10 @@ makeCacheMatrix <- function(x = matrix()) {
          )
 }
 
-## Write a short comment describing this function
-
+## This function returns the inverse of the special matrix, created 
+## with the function makeCacheMatrix.
+## The inverse will be calculated or, if the calculation already have been done before,
+## retrieve from the cache.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
     im <- x$getInvMat()
@@ -28,6 +35,7 @@ cacheSolve <- function(x, ...) {
         message("getting cached data")
         return(im)
     }
+    message("calculating inverse matrix")
     data <- x$get()
     im <- solve(data, ...)
     x$setInvMat(im)
